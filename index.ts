97b536827979abbfd9f4e887e61e8d739c3890d4
Johnny Wu <karasaya@gmail.com>
2026-05-12 18:49:24 +0800

    chore: migrate peerDependency to @earendil-works/pi-coding-agent

--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@
 import type {
   ExtensionAPI,
   ExtensionContext,
-} from '@mariozechner/pi-coding-agent';
+} from '@earendil-works/pi-coding-agent';
 
 const OSC = '\x1b]';
 const BEL = '\x07';
